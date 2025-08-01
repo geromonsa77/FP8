08343b4ecc47b35dd79489703869df011312f56a
Geronimo Monsalve Jaramillo <geronimo.monsalve@upb.edu.co>
2025-08-01 09:35:27 -0500

    Variables, Constantes y tipos de datos

---
+++ 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _1.InicioVisualStudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración e inicialización de variables
+            string nombre = "gero";
+            const string iva = "19%";
+            string apellidos = "Monsalve Jaramillo";
+            Console.Write(" Bienvenidos al curso de Fundamentos de Programación\n");
+            nombre = "Geronimo";
+            //iva = "20%"; No puede cambiar el valor de "iva" por ser una constante
+            Console.Write("\t" + nombre + " " + apellidos + " " + iva);
+            Console.ReadKey();
+        }
+    }
+}
