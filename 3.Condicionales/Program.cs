bc6a60ad5087394fccda8f3b61ec194d2de708b1
geromonsa77 <geronimo.monsalve@upb.edu.co>
2025-08-13 08:14:56 -0500

    Condicionales simples, queda un ejercicio para la proxima clase

---
+++ 3.Condicionales/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _3.Condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingrese el nombre");
+            nombre = Console.ReadLine(); //Todo lo que se lee desde la consola llega como un string
+
+            Console.WriteLine("Ingrese el sueldo");
+            sueldo = Convert.ToInt32(Console.ReadLine());
+
+            if (sueldo > 3000)
+            {
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+                //Console.WriteLine("La persona" + nombre + ", debe abonar impuestos");
+            }
+
+            /* 2. Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, se debe mostrar el siguiente mensaje: “Bienvenido a mi sitio web”. */
+            int edad = 0;
+            Console.WriteLine("Ingrese su edad");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad > 18)
+            {
+                Console.WriteLine("Bienvenido a mi sitio web");
+            }
+
+
+        }
+    }
+}
