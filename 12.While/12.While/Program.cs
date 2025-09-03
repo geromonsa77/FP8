cd0b7b68884b8dcdb2ec395ec990c28f847951a3
geromonsa77 <geronimo.monsalve@upb.edu.co>
2025-09-03 08:12:02 -0500

    12 while

---
+++ 12.While/12.While/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _12.While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;     
+            int acumulador = 0;   
+
+            while (contador <= 99)
+            {
+                acumulador += contador;   
+                contador += 2;            
+            }
+
+            Console.WriteLine("La suma de los números impares hasta 99 es: " + acumulador);
+            
+        }
+    }
+}
