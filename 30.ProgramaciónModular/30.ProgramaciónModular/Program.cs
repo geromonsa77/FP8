49ab1b1e81bf40c1c16253b26136c7fb21d6c2c0
geromonsa77 <geronimo.monsalve@upb.edu.co>
2025-10-16 18:01:31 -0500

    30.ProgramaciónModular

---
+++ 30.ProgramaciónModular/30.ProgramaciónModular/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _30.ProgramaciónModular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje();
+            MostrarMensajes("Mi nombre es Geronimo");
+            MostrarMensajes("Tengo 18 años");
+            Console.WriteLine($"Edad calculada: {CalcularEdad()}");
+            MostrarMensajes($"Tengo {CalcularEdad(2025, 2007)} años");
+        }
+        //Módulo 1 - Procedimiento sin parámetros
+        static void MostrarMensaje()
+        {
+            Console.WriteLine("Bienvenidos a la clase de programación modular");
+            MostrarMensajes("Estoy desde procedimiento sin parámetros");
+        }
+
+        //Módulo 2 - Procedimiento con parámetros
+
+        static void MostrarMensajes(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+
+        //Módulo 3 - Funciones
+
+        static int CalcularEdad()
+        {
+            int edad;
+            edad = 2025 - 2007;
+
+            return edad;
+        }
+
+        //Mósulo 4 - Funciones con parámetros
+
+        static int CalcularEdad(int AñoActual, int AñoNacimiento)
+        {
+            return AñoActual - AñoNacimiento;
+        }
+    }
+}
+
